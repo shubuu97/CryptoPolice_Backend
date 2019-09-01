5600c8056f535f0672a38c53fea8c90765f71ad6
Shubham Chitransh <shubhamchitransh8@gmail.com>
2019-09-01 01:51:37 -0700

    done

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var firebase = require("firebase-admin");
+var app = express();
+app.use(bodyParser.json());
+
+var serviceAccount = require("./backend-36be7-firebase-adminsdk-w8abz-53553f1538.json");
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://backend-36be7.firebaseio.com"
+});
+
+app.use(cors());
+var db = firebase.database();
+var ref = db.ref("formData");
+
+app.post("/save/formdata", function(req, res) {
+  let body = req.body;
+  ref.set(body);
+  res.status(200).send("Data Saved Sucessfully!");
+});
+
+app.listen(8080, function() {
+  console.log("CORS-enabled web server listening on port 8080");
+});
